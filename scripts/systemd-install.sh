1cf3c8aab1d6fa3cfc0b0c1297ade66480f489e3
David <david.ng.dev@gmail.com>
2026-01-20 18:41:25 +0000

    fix install script

--- scripts/systemd-install.sh
+++ scripts/systemd-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-deno task -f "@app/server" systemd-install
-deno task -f "@app/web-client" systemd-install
+deno task -f "@app/server" systemd:install
+deno task -f "@app/web-client" systemd:install
